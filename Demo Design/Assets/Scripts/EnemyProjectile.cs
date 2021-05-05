6bc2a154a53fe4bd0127ac918d229758ee6584b9
F. Eren Dalçık <dalcikferhateren@gmail.com>
2021-05-05 11:34:25 +0300

    Adding project files.

---
+++ Demo Design/Assets/Scripts/EnemyProjectile.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+	// Start is called before the first frame update
+	
+	private Rigidbody2D rigidBody;
+
+    public GameObject EnemySpawner;
+   
+    public GameObject enemyProjectile;
+
+    public float speed = 22;
+    Vector3 respawn = new Vector3(0, -2, 0);
+	
+    void Start()
+	{
+		rigidBody = GetComponent<Rigidbody2D>();
+
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, -10 * Time.deltaTime, 0));
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.position = respawn;
+            
+            Destroy(enemyProjectile);
+           
+            GameManager.lives--;
+            
+
+        }
+        if (collision.gameObject.tag == "Homeship")
+        {
+           
+            Destroy(enemyProjectile);
+
+            GameManager.lives--;
+
+
+        }
+        if (collision.gameObject.tag == "Platform")
+        {
+
+            Destroy(enemyProjectile);
+        }
+        if (collision.gameObject.tag == "Wall")
+        {
+
+            Destroy(enemyProjectile);
+        }
+    }
+}
---
+++ Demo Design/Assets/Scripts/Projectile1.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Projectile1 : MonoBehaviour
+{
+    public GameObject projectile;
+    public GameObject enemyProjectile;
+
+    public Text Score;
+
+    public GameObject ExpAnimator;
+    public AudioClip destroySound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 8 * Time.deltaTime, 0));
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            Destroy(projectile);
+            Destroy(enemyProjectile);
+
+            EnemySpawner.score = EnemySpawner.score + 5;
+            EnemySpawner.enemyCount--;
+
+            ExpAnimator.gameObject.SetActive(true);
+
+        }
+
+        if (collision.gameObject.tag == "Wall")
+        {
+            
+            Destroy(projectile);
+        }
+
+        if (collision.gameObject.tag == "Platform")
+        {
+            
+            Destroy(projectile);
+        }
+        if (collision.gameObject.tag == "Enemy Projectile")
+        {
+
+            Destroy(projectile);
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag == "Outlier")
+        {
+
+            Destroy(projectile);
+        }
+    }
+
+
+   
+}
